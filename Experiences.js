c97324c57b3c02f5026a35312299c54d6723ccf2
WolvesareElite123 <WolvesareElite123@users.noreply.github.com>
2022-05-25 17:48:10 -0700

    Completed Experiences.html HTML + Unlocked button for main page

---
+++ Experiences.js
@@ -0,0 +1,57 @@
+﻿//Filter Related
+function applyFilters()
+{
+
+}
+
+function toggleInt()
+{
+
+}
+
+function toggleProj()
+{
+
+}
+
+function toggleOrg()
+{
+
+}
+
+
+//Sort Related
+document.getElementById("compName").addEventListener("click", sortCompany);
+document.getElementById("compTime").addEventListener("click", sortTime);
+document.getElementById("compTitle").addEventListener("click", sortTitle);
+document.getElementById("compGroup").addEventListener("click", sortDept);
+
+document.getElementById("projName").addEventListener("click", sortDept);
+document.getElementById("projTime").addEventListener("click", sortDept);
+document.getElementById("projTitle").addEventListener("click", sortDept);
+
+
+function sortCompany()
+{
+    constructAlert();
+}
+
+function sortTime()
+{
+    constructAlert();
+}
+
+function sortTitle()
+{
+    constructAlert();
+}
+
+function sortDept()
+{
+    constructAlert();
+}
+
+function constructAlert()
+{
+    window.alert("Sorting is currently unavailable");
+}
