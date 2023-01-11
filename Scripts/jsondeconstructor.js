646e41421d973429b10109cea4218ba6093fadf6
WolvesareElite123 <WolvesareElite123@users.noreply.github.com>
2023-01-11 16:19:59 -0600

    transfer

---
+++ Scripts/jsondeconstructor.js
@@ -0,0 +1,183 @@
+﻿
+var brace = '{}';
+var sep = ',';
+var keysep = ':';
+var target = '';
+var input = '';
+
+//Cleaned
+var targetList = [];
+var leftBrace = '{';
+var rightBrace = '}';
+
+
+function run()
+{
+    retrieveData();
+    cleanData();
+    runData();
+}
+
+function retrieveData()
+{
+    brace = document.getElementById('inputBrace').value;
+    sep = document.getElementById('inputSeparator').value;
+    keysep = document.getElementById('inputkeySeparator').value;
+    target = document.getElementById('targets');
+    input = document.getElementById('input').value;
+    if(target.value.length==0)
+    {
+        target.value = "";
+    }
+    else
+    {
+        target = target.value;
+    }
+}
+
+function cleanData()
+{
+    brace = brace.replace(/\s/g, "");
+    sep = sep.replace(/\s/g, "");
+    keysep = keysep.replace(/\s/g, "");
+    target = target.toString().replace(/\s/g, "");
+    input = input.replace(/\s+/g, "").trim();
+    input = input.replace(/['"]+/g, '');
+    
+    //Clean Target
+    targetList = target.split(',');
+    
+    //Clean Brace
+    leftBrace = brace.substring(0,brace.length/2)
+    rightBrace = brace.substring(brace.length/2)
+}
+
+function runData()
+{
+    let count = 0; //Count layer
+    let isKey = true; //False if we're on Value
+    let lastKey = '';
+    let lastVal = '';
+    let currentKeyPath = [];
+    let keyID = 0;
+    for(let x = 0; x<input.length; x++)
+    {
+        //Progress Bar
+        let currentProgress = ((x+1)/input.length)*100;
+        currentProgress = Math.round(currentProgress);
+        $('#progress').attr('aria-valuenow', currentProgress).css('width', currentProgress+'%');
+        
+        //Actual work
+        
+        //Detect Layer
+        let char = input.substring(x,x+1);
+        if(char === leftBrace) //New Layer
+        {
+            currentKeyPath.push(lastKey);
+            let currentValue = document.getElementById('out1').value;
+            document.getElementById('out1').value = currentValue + "\r\n";
+            count++;
+            continue;
+        }
+        else if(char === rightBrace) //End Layer
+        {
+            currentKeyPath.pop();
+            count--;
+            continue;
+        }
+        
+        //Detect Text
+        //if(char.toLowerCase()===char.toUpperCase()) //Detect is letter
+        if(char.match(/[a-z]/i))
+        {
+            let keyStr = char;
+            let valStr = '';
+            let loc = x+1;
+            while(char!==sep && char!==rightBrace)
+            {
+                char = input.substring(loc, loc+1);
+                if(isKey)
+                {
+                    keyStr += char;
+                }
+                else
+                {
+                    valStr += char;
+                }
+                loc++;
+            }
+            x = loc-1;
+
+            let htmlString ="";
+            for(let y = 0; y<count; y++)
+            {
+                htmlString+=">" + "    ";
+            }
+            htmlString += ">" + keyStr + " : " + valStr + "\r\n";
+            //$('#out1').append(htmlString);
+            let currentValue = document.getElementById('out1').value;
+            document.getElementById('out1').value = currentValue + htmlString;
+            lastKey = keyStr;
+            lastVal = valStr;
+            
+            
+            /*
+            let htmlString ="<p class='display-6' id=''>";
+            for(let y = 0; y<count; y++)
+            {
+                htmlString+=">" + "&emsp";
+            }
+            htmlString +=
+                ">" + keyStr + " : " + valStr +
+            "</p>";
+            $('#out1').append(htmlString);
+            lastKey = keyStr;
+            lastVal = valStr;
+             */
+        }
+        
+        //Target?
+        if(targetList.length>0)
+        {
+            if(targetList.indexOf(lastKey) > -1)
+            {
+                //Print Path
+
+                let htmlString ="";
+                for(let y = 0; y<count; y++)
+                {
+                    htmlString+= "[" + currentKeyPath[y] + "]" + " > ";
+                }
+                htmlString += currentKeyPath[currentKeyPath.length-1];
+                //$('#out1').append(htmlString);
+                let currentValue = document.getElementById('out2').value;
+                document.getElementById('out2').value = currentValue + htmlString;
+                
+                
+                /*
+                let htmlString = "<p class='display-6' id=''>";
+                for(let y = 0; y<currentKeyPath.length-1; y++)
+                {
+                    htmlString+= "[" + currentKeyPath[y] + "]" + " > ";
+                }
+                htmlString += "<mark><u>" + currentKeyPath[currentKeyPath.length-1] + "</u></mark>"
+                $('#out2').append(htmlString);
+                 */
+            }
+        }
+        
+        //ERROR
+        if(char.toLowerCase()!==char.toUpperCase())
+        {
+            runFailed("runData", "UNKNOWN CHARACTER: " + char);
+            break;
+        }
+    }
+}
+
+function runFailed(location, msg)
+{
+    window.alert('FAILURE DETECTED IN: ' + location + ' DUE TO: ' + msg);
+    window.alert('IF YOU BELIEVE THERE IS AN ERROR, PLEASE CONTACT THE DEVELOPER');
+    return;
+}
